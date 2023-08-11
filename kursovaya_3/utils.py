32b4a8d866a63aecdc2edaf120818c4c0249f152
Eugen <eugenschmigel@gmail.com>
2023-08-11 16:46:35 +0200

    developing the logic - read, validate und filter the data from JSON file "operations.json"

---
+++ kursovaya_3/utils.py
@@ -0,0 +1,31 @@
+import json
+from pprint import pprint
+
+
+with open('operations.json', encoding='utf-8') as operations_file:
+
+    data = json.load(operations_file)
+
+def sorted_by_date(data):
+    validate_operation = []
+    for operation in data:
+        if not operation:
+            continue
+        elif not 'date' in operation:
+            continue
+        elif not operation.get('date'):
+            continue
+        validate_operation.append(operation)
+
+        if 'date' in operation and operation['date']:
+            validate_operation.append(operation)
+    return validate_operation
+
+validated_operation = sorted_by_date(data)
+
+def sort_data(validated_operation):
+    sorted_data = sorted(validated_operation, key=lambda x: x['date'], reverse=True)
+    return pprint(sorted_data)
+
+sort_data(validated_operation)
+
